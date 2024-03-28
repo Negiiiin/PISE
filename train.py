7a7f6edaaa5d5b64e63cfa9e6f295522381ceaf2
Negin Baghbanzadeh <negin@UIT-CSS-H96V2D3.yorku.yorku.ca>
2024-03-28 16:42:01 -0400

    updated train.py

--- train.py
+++ train.py
@@ -1,15 +1,34 @@
 import time
 import data as Dataset
-from model import create_model
+import torch
+from . import visualizer
 
+
+import utils
 
 opt = {
     "batchSize": 16,
     "serial_batches": False,
     "nThreads": 8,
     "isTrain": True,
-    "max_epochs": 40
+    "max_epochs": 40,
+    "gpu_ids": 0,
+    "display_id": 0,
+    "display_winsize": 256,
+    "name": "experiment_name",
+    "no_html": True,
+    # "display_env":,
+    "display_single_pane_ncols": 0,
+    "checkpoints_dir": "./results",
+    "save_iters_freq": 5000
 }
+
+if torch.cuda.is_available():
+    device = torch.device('cuda:0')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')  # Use MPS on supported Macs
+else:
+    device = torch.device('cpu')
 
 dataset = Dataset.create_dataloader(opt)
 
@@ -20,16 +39,25 @@ keep_training = True
 
 epoch = 0
 
+visualizer = visualizer.Visualizer(opt)
+model = utils.Final_Model(gpu_ids=opt["gpu_ids"], device=device, save_dir=opt["checkpoints_dir"])
+
 # training process
 while (epoch < opt["max_epochs"]):
     epoch_start_time = time.time()
     epoch += 1
     print('\n Training epoch: %d' % epoch)
 
+
     for i, data in enumerate(dataset):
         iter_start_time = time.time()
-        # model.set_input(data) #TODO
-        # model.optimize_parameters() #TODO
+        model.set_input(data)
+        model.optimize_parameters()
+
+        # save the model every <save_iter_freq> iterations to the disk
+        if i % opt["save_iters_freq"] == 0:
+            print('saving the model of iterations %d at epoch %d' % (i, epoch))
+            model.save_networks(epoch*len(dataset)+i)
 
 """
         # display images on visdom and save images
@@ -58,11 +86,6 @@ while (epoch < opt["max_epochs"]):
         if total_iteration % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' % (epoch, total_iteration))
             model.save_networks('latest')
-
-        # save the model every <save_iter_freq> iterations to the disk
-        if total_iteration % opt.save_iters_freq == 0:
-            print('saving the model of iterations %d' % total_iteration)
-            model.save_networks(total_iteration)
 """
 
     # model.update_learning_rate() # TODO
